1e098384cb363e9b5f3a5b17bfc5ff8c2b44b361
Reed Santos <reedsantos@icloud.com>
2020-10-31 17:32:56 -0400

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "../styles/Header.css";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Employee Directory</h1>
+                <p>Click the header "Name" to sort by name and use the search bar to filter employees.</p>
+            </div>
+        )
+    }
+}
