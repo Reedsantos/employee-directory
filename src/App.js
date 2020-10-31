396f3f87239a63c101d88e0e1483babf9236ad1f
Reed Santos <reedsantos@icloud.com>
2020-10-31 18:41:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Header from "./components/Header";
+import Main from "./components/Main";
 import './App.css';
 import Wrapper from './components/Wrapper';
 
 function App() {
-    return (
-        <div className="App">
-            <Wrapper>
-                <Header />
-            </Wrapper>
-
-        </div>
-    );
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
+  );
 }
 
 export default App;
